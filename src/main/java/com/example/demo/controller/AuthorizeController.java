cde8c00372923a97ccd4484309b9c9cb837eb716
duuang <15201692553@126.com>
2021-05-02 00:58:05 +0800

    add function to retrieve user data

---
+++ src/main/java/com/example/demo/controller/AuthorizeController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+import com.example.demo.dto.AccessTokenDTO;
+import com.example.demo.dto.GithubUser;
+import com.example.demo.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callBack(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("Iv1.f4886294ce16b013");
+        accessTokenDTO.setClient_secret("25e3eb4c7f7bd1a58abf000ba42f2523746ed8ae");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        System.out.println(accessToken);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        System.out.println(user.getId());
+        return "index";
+    }
+}
